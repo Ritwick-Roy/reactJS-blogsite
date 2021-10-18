d84d753b1d7af07cdc999d310c637c11e545bf95
Ritwick-Roy <ritwickroy2002@gmail.com>
2021-10-19 03:39:33 +0530

    useState hook and lists

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,36 @@
+import {useState} from 'react'
+import BlogList from './BlogList';
+
 const Home = () => {
-    const buttonClick = (e) =>{
-        console.log("Clicked!",e);
-    };
-    const buttonClickAgain =(name,e)=>{
-        console.log('Clicked again! '+name,e.target);
-    };
+    // let name='MLSC';
+    // const clickButton = () =>{
+    //     name='MLSC DB';
+    //     console.log(name);
+    // }
+    // const [name,setName]=useState('MLSC');  //this useState is a hook
+    // const [place,setPlace]=useState('Patiala');
+    // const clickButton = () =>{
+    //     setName('MLSC DB');
+    //     setPlace('Derabassi');
+    // }
+    const [blogs,setBlogs]=useState([
+        {title:"react",body:"this",author:"mario",id:1},
+        {title:"java",body:"means",author:"luigi",id:2},
+        {title:"script",body:"nothing",author:"mario",id:3}
+    ]);
+
+    const handleDelete = (id) =>{
+        const newBlogs=blogs.filter((blog)=>blog.id !== id)
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="Home">
-            <h2>Homepage</h2>
-            <button onClick={buttonClick}>CLICK ME!</button>
-            <button onClick={(e)=>buttonClickAgain('mario',e)}>CLICK ME!</button> 
+            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog)=> blog.author==='mario')} title="Mario's blogs!" handleDelete={handleDelete}/>
         </div>
     );
-    //we called an anonymous function above because it would have called the function if we used parentheses s
 }
- 
+ //props is used for transferring data from parent component to child component
+ //we can obtain the data transferred to a child with 'props' on the child which has all the data attached
 export default Home;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>  {/* this means that react does additional checks during development and gives us warnings in console if necessary  */}
+    <App />                   {/* name has to start with a capital letter */}
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,4 +14,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
